4c6050a4d5b932b9fce31bf9eb290c8f67f5d1e0
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-08-27 16:57:09 -0300

    Fill drawing metadata with defaults

--- src/drawing/canvas.cljs
+++ src/drawing/canvas.cljs
@@ -1,9 +1,14 @@
 (ns drawing.canvas
   (:require [goog.dom :as dom]))
 
+(def default-drawing-mt {:name   "drawing"
+                         :width  600
+                         :height 600})
+
 (defn draw*
   ([f] (draw* {} f))
-  ([mt f] (let [{:keys [width height]} mt
+  ([mt f] (let [mt (merge default-drawing-mt mt)
+                {:keys [width height]} mt
                 id (name (:name mt))]
             (when-not (dom/getElement id)
               (dom/append js/document.body (dom/createDom "canvas" #js {:id id})))
