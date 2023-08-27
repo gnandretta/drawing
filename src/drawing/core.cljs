0190d34702b4a136438c5ae28b8516a66018b994
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-08-27 11:04:21 -0300

    Draw something to test code reload

--- src/drawing/core.cljs
+++ src/drawing/core.cljs
@@ -1,4 +1,13 @@
-(ns drawing.core)
+(ns drawing.core
+  (:require [goog.dom :as dom]))
 
 (defn ^:dev/after-load init []
-  (println "drawing"))
+  (let [id "drawing"
+        width 608
+        height 1080]
+    (when-not (dom/getElement id)
+      (dom/append js/document.body (dom/createDom "canvas" #js {:id id})))
+    (let [canvas (dom/getElement id)
+          ctx (.getContext canvas "2d")]
+      (dom/setProperties canvas #js {:width width :height height})
+      (.fillRect ctx 0 0 width height))))
