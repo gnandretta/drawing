42aa1ed845a96ea530fb163c057c0a1fcb07a11c
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-08-28 12:00:15 -0300

    Let set-fill-style use a dynamic canvas context

--- src/drawing/canvas.cljs
+++ src/drawing/canvas.cljs
@@ -2,6 +2,8 @@
   (:require [goog.object :as object]
             [goog.dom :as dom]))
 
+(def ^:dynamic *ctx* nil)
+
 (def default-drawing-mt {:name   "drawing"
                          :width  600
                          :height 600})
@@ -13,14 +15,16 @@
                 id (name (:name mt))]
             (when-not (dom/getElement id)
               (dom/append js/document.body (dom/createDom "canvas" #js {:id id})))
-            (let [canvas (dom/getElement id)]
+            (let [canvas (dom/getElement id)
+                  ctx (.getContext canvas "2d")]
               (dom/setProperties canvas #js {:width width :height height})
-              (f {:ctx    (.getContext canvas "2d")
-                  :width  width
-                  :height height})))))
+              (binding [*ctx* ctx]
+                (f {:ctx    ctx
+                    :width  width
+                    :height height}))))))
 
 (defn set-fill-style
   "Sets the CSS color, gradient, or pattern to use inside shapes. \"#000\" by
    default."
-  [ctx value]
-  (object/set ctx "fillStyle" value))
+  [value]
+  (object/set *ctx* "fillStyle" value))
