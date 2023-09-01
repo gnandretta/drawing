7eaf0b4664751796a5ddfc36fdccc628fd7c9d1f
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-09-01 17:15:31 -0300

    Inline hi and ho

--- src/drawing/core.cljs
+++ src/drawing/core.cljs
@@ -13,19 +13,17 @@
     (c/fill-rect [0 0] (d))
     (c/translate (d 0.5))
     (doseq [_ (range 45000)]
-      (let [a (rand (* 2 js/Math.PI))
-            hi (* (- 1 (rand (rand))) r)
-            ho (* (js/Math.cosh (rand js/Math.PI)) r)]
+      (let [a (rand (* 2 js/Math.PI))]
         (c/set-fill-style (m/weighed-rand-key {"rgb(109,79,246)"  4
                                                "rgb(64,0,131)"    2
                                                "rgb(57,0,3)"      2
                                                "rgb(255,195,190)" 2
                                                "rgb(255,255,255)" 1}))
         (when (<= (rand) pi)
-          (c/fill-rect (m/sides a hi)
+          (c/fill-rect (m/sides a (* (- 1 (rand (rand))) r))
                        [1 1]))
         (when (<= (rand) po)
-          (c/fill-rect (m/sides a ho)
+          (c/fill-rect (m/sides a (* (js/Math.cosh (rand js/Math.PI)) r))
                        [1 1]))))))
 
 (defn ^:dev/after-load init []
