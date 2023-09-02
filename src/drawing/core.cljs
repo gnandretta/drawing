fd67eb617d54a7fd8163057e5b39c1e08fd46e2c
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-09-02 11:58:00 -0300

    Update drawing to take less params
    The previous doc didn't make much sense and this is the result of trying
    to make it easier to understand. However, now there's an explicit
    relation between the number of points drawn inside and outside the
    circle.

--- src/drawing/core.cljs
+++ src/drawing/core.cljs
@@ -4,21 +4,19 @@
             [goog.object :as object]))
 
 (defn drawing
-  "Randomly draws at most c points inside and at most c points outside a circle
-   of radius r, determined by the probabilities pi and po, respectively. fhi
-   and fho are fns that take no arguments and return random hypotenuses for the
-   points inside and outside the circle, fhi codomain is [0,1] and fho codomain
-   is >= 1 (although large values won't fit in the canvas). bg is the color of
-   the background and fgs is a map with colors as keys and weights as values,
-   the colors of the points are picked from that map."
+  "Randomly draws c points inside or outside a circle of radius r on a
+   background bg. The colors of the points are picked from the fgs map, which
+   has colors as keys and weights as values. The position of the point is
+   determined by the hypotenuse function fh which takes no arguments and returns
+   a number—[0,1] inside the circle, and >= 1 outside (which may not fit in the
+   canvas)."
   {:dimensions [608 1080]}
-  [{:keys [r c pi fhi po fho bg fgs]
+  [{:keys [r c fh bg fgs]
     :or   {r   0.3
            c   45000
-           pi  (rand)
-           fhi #(- 1 (rand (rand)))
-           po  (rand)
-           fho #(js/Math.cosh (rand js/Math.PI))
+           fh #(if (< (rand) 0.5)
+                 (- 1 (rand (rand)))
+                 (js/Math.cosh (rand js/Math.PI)))
            bg  "rgb(229,228,228)"
            fgs {"rgb(109,79,246)"  4
                 "rgb(64,0,131)"    2
@@ -31,10 +29,7 @@
   (doseq [_ (range c)]
     (let [a (rand (* 2 js/Math.PI))]
       (c/set-fill-style (m/weighed-rand-key fgs))
-      (when (<= (rand) pi)
-        (c/fill-rect (m/sides a (w (fhi) r)) [1 1]))
-      (when (<= (rand) po)
-        (c/fill-rect (m/sides a (w (fho) r)) [1 1])))))
+      (c/fill-rect (m/sides a (w (fh) r)) [1 1]))))
 
 (defn ^:dev/after-load init []
   (c/draw drawing))
