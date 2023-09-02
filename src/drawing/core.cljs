93379d14b8971961ef5d6db540914be3a135e203
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-09-02 11:09:26 -0300

    Parameterize hypotenuse fns

--- src/drawing/core.cljs
+++ src/drawing/core.cljs
@@ -5,10 +5,12 @@
 
 (defn drawing
   {:dimensions [608 1080]}
-  [{:keys [r pi po bg fgs]
+  [{:keys [r pi fhi po fho bg fgs]
     :or   {r   0.3
            pi  (rand)
+           fhi #(- 1 (rand (rand)))
            po  (rand)
+           fho #(js/Math.cosh (rand js/Math.PI))
            bg  "rgb(229,228,228)"
            fgs {"rgb(109,79,246)"  4
                 "rgb(64,0,131)"    2
@@ -22,11 +24,9 @@
     (let [a (rand (* 2 js/Math.PI))]
       (c/set-fill-style (m/weighed-rand-key fgs))
       (when (<= (rand) pi)
-        (c/fill-rect (m/sides a (w (- 1 (rand (rand))) r))
-                     [1 1]))
+        (c/fill-rect (m/sides a (w (fhi) r)) [1 1]))
       (when (<= (rand) po)
-        (c/fill-rect (m/sides a (w (js/Math.cosh (rand js/Math.PI)) r))
-                     [1 1])))))
+        (c/fill-rect (m/sides a (w (fho) r)) [1 1])))))
 
 (defn ^:dev/after-load init []
   (c/draw drawing))
