c843eb56deed251fab296ff637beb0293b2a62b3
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-09-16 16:18:21 -0300

    Split fh param into p fho fhi

--- src/drawing/core.cljs
+++ src/drawing/core.cljs
@@ -1,21 +1,25 @@
 (ns drawing.core
   (:require [drawing.canvas :as c :refer [d w] :include-macros true]
-            [drawing.math :as m]))
+            [drawing.math :as m]
+            [noc.randomness :as noc]))
 
 (defn drawing
   "Randomly draws c points inside or outside a circle of radius r on a
    background bg. The colors of the points are picked from the fgs map, which
-   has colors as keys and weights as values. The position of the point is
-   determined by the hypotenuse function fh which takes no arguments and returns
-   a number—[0,1] inside the circle, and >= 1 outside (which may not fit in the
-   canvas)."
+   has colors as keys, and weights as values. The position of the point is
+   determined by the hypotenuse functions fhi and fho which take no arguments
+   and (must) return a number between [0,1] (inside the circle) and >= 1
+   (outside), respectively. The probability of using fhi for a point is p, and
+   the probability of using fho is 1-p. Note that two or more points may be
+   drawn in the same coordinate and that some points may not fit the canvas
+   (when fho returns a number large enough)."
   {:size [608 1080]}
-  [{:keys [r c fh bg fgs]
+  [{:keys [r c p fhi fho bg fgs]
     :or   {r   0.3
            c   45000
-           fh  #(if (< (rand) 0.5)
-                  (- 1 (rand (rand)))
-                  (js/Math.cosh (rand js/Math.PI)))
+           p   0.7
+           fhi #(- 1 (rand (rand)))
+           fho #(js/Math.cosh (rand js/Math.PI))
            bg  "rgb(229,228,228)"
            fgs {"rgb(109,79,246)"  4
                 "rgb(64,0,131)"    2
@@ -28,7 +32,7 @@
   (doseq [_ (range c)]
     (let [a (rand (* 2 js/Math.PI))]
       (c/set-fill-style (m/weighed-rand-key fgs))
-      (c/fill-rect (m/sides a (w (fh) r)) [1 1]))))
+      (c/fill-rect (m/sides a (w ((if (< (rand) p) fhi fho)) r)) [1 1]))))
 
 (defn ^:dev/after-load init []
   (c/draw drawing))
