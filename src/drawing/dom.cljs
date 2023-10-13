e418c4a8c8525282a36589ef58ccebe5cebf0f52
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-10-13 18:07:27 -0300

    Let events take the output channel

--- src/drawing/dom.cljs
+++ src/drawing/dom.cljs
@@ -1,7 +1,8 @@
 (ns drawing.dom
   (:require [cljs.core.async :refer [chan put!]]))
 
-(defn events [target type]
-  (let [out (chan)]
+(defn events
+  ([target type] (events target type (chan)))
+  ([target type out]
     (.addEventListener target type (fn [e] (put! out e)))
     out))
