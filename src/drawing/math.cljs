8e859decb2a8fd14119b97f2b93e87cb0298eb21
Gabriel Andretta <gabriel.andretta@gmail.com>
2023-11-18 11:40:07 -0300

    Add sign fn

--- src/drawing/math.cljs
+++ src/drawing/math.cljs
@@ -42,6 +42,11 @@
   [v]
   (div v (mag v)))
 
+(defn sign
+  "Returns n if positive? is true and -n otherwise."
+  [n positive?]
+  (cond-> n (not positive?) (* -1)))
+
 (defn lerp
   "Maps n from [a,b] (defaults to [0,1]) to [c,d] with a linear interpolation."
   ([n [c d]] (lerp n [0 1] [c d]))                          ; also works when d < c
